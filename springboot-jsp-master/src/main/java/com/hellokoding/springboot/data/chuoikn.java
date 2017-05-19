3514b6411e6207a242b97eecff90e7f01de4c9e2
ThanhNgocT3N <14110132@student.hcmute.edu.vn>
2017-05-19 10:13:36 +0700

    Update chuoikn.java

--- springboot-jsp-master/src/main/java/com/hellokoding/springboot/data/chuoikn.java
+++ springboot-jsp-master/src/main/java/com/hellokoding/springboot/data/chuoikn.java
@@ -4,11 +4,10 @@ public class chuoikn {
 	public String name="root";
 	public String pass="1234";
 
-	/*String sql = String.format(
+	String sql = String.format(
 	        "jdbc:mysql://google/%s?cloudSqlInstance=%s&"
-	            + "socketFactory=com.google.cloud.sql.mysql.SocketFactory", "dtdmnhom9","feisty-circuit-161108:asia-northeast1:dtdmnhom9");
-*/
-	String sql ="jdbc:mysql://localhost/hello";
+	            + "socketFactory=com.google.cloud.sql.mysql.SocketFactory", "hello","feisty-circuit-161108:asia-northeast1:hello");
+	//String sql ="jdbc:mysql://localhost/hello";
 public String getSQL()
 {
 	return sql;
